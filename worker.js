3736a48ef8e75e7a3dc4340c578a57b45e0682ac
axcellsz <122135545+axcellsz@users.noreply.github.com>
2025-11-29 11:47:18 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,18 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
 
+export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await getAssetFromKV(
+        { request, waitUntil: p => ctx.waitUntil(p) },
+        {
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: __STATIC_CONTENT_MANIFEST,
+        }
+      );
+    } catch (e) {
+      // kalau file tidak ketemu, bisa lanjut ke logic API kamu
+      return new Response("Not found", { status: 404 });
+    }
+  },
+};
